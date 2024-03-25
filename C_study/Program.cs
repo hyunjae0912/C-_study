aeac87917abb9b55824051b26c236196808ef8ac
hyunjae0912 <hyunjae00912@naver.com>
2024-03-25 17:39:17 +0900

    self_study

---
+++ C_study/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_study
+{
+
+    class globla
+    {
+        public static int count = 0;
+    }
+
+    class ClassA
+    {
+        public ClassA() {
+            globla.count++;
+        }
+    }
+
+    class ClassB{
+        public ClassB()
+        {
+            globla.count++;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(globla.count);
+
+            new ClassA();
+
+
+            Console.WriteLine(globla.count);
+
+            new ClassB();
+
+            Console.WriteLine(globla.count);
+
+        }
+    }
+}
